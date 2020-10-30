d4c5bd515aeaaf601d02026d77c7d728353d8c83
朱嘉豪 <zhujiahao@MacBook-Pro.local>
2020-10-30 17:55:46 +0800

    update

--- init.lua
+++ init.lua
@@ -1,24 +1,12 @@
 --导入配置文件
 require 'config'
 
---设置局部变量
-local whiteIpButton	=	optionIsOn(whiteIpButton)
-local remoteIp 		= 	nil
---获取远程IP地址
-function getRemoteIp()
-	remoteIp=ngx.var.remote_addr
-	if not remoteIp then
-		remoteIp = "unknow"
-	end
-end
-getRemoteIp()
+--导入工具函数
+require 'tools'
 
-
---判断开关状态
-function optionIsOn(option)
-	-- body
-	return option == "on"
-end
+--设置变量
+remoteIp 		= 	getRemoteIp()
+whiteIpButton	=	optionIsOn(whiteIpButton)
 
 
 --IP白名单检测
