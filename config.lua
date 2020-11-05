d83118f9db8c7a1a6ffe5f6756ebae5ffc1c8130
朱嘉豪 <zhujiahao@MacBook-Pro.local>
2020-11-05 15:36:59 +0800

    update

--- config.lua
+++ config.lua
@@ -1,6 +1,10 @@
 --waf config file
 
-config_waf_rules_path	=	"/opt/openresty/mywaf/"
+--waf rules path
+config_waf_rules_path	=	"/opt/openresty/mywaf/rules"
+
+--waf logs path
+config_waf_logs_path	=	"/opt/openresty/mywaf/logs"
 
 --waf status
 config_waf_status		=	"on"
